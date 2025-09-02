61f3e3ef159f188d97787ef220b3c192dda7e1fe
Kefentse-Mfoloe <kefentse.mfoloe@gmail.com>
2025-09-02 22:08:51 +0200

    Add icons to sidebar +  add album cover image

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,16 @@ import './App.css'
 function App() {
   return (
     <div id="grid-container">
-      <div className="aside"></div>
+      <div className="aside">
+        <i className="fa fa-bars"></i>
+        <i className="fa fa-home"></i>
+        <i className="fa fa-search"></i>
+        <i className="fa fa-volume-up"></i>
+        <i className="fa fa-user"></i>
+        <i className="fa fa-spotify"></i>
+        <i className="fa fa-facebook-f"></i>
+        <i className="fa fa-cog"></i>
+      </div>
       <div className="main"></div>
       <div className="footer"></div>
     </div>

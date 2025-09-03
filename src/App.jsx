29d725011a9af2478dd53a8b77c050e43fbf5a71
Kefentse-Mfoloe <kefentse.mfoloe@gmail.com>
2025-09-03 22:22:03 +0200

    Add main header + content

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import albumCover from './assets/default_album_cover.jpg'
 
 
 function App() {
@@ -17,7 +18,26 @@ function App() {
         <i className="fa fa-facebook-f"></i>
         <i className="fa fa-cog"></i>
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <div className="main_header">
+          <div className="img">
+            <img src={albumCover} alt="Album Cover" />
+          </div>
+          <section className="details">
+            <div>
+              <p>good kid, m.A.A.d city (Deluxe)</p>
+              <p className="sm-hide">Kendrick Lamar</p>
+              <p className="sm-hide">2012 . 1 hr 32 min . 17 songs</p>
+            </div>
+            <div>
+              <i className="fa fa-play"> &nbsp;Play all</i>
+              <i className="fa fa-plus"> &nbsp;Add to</i>
+              <i className="fa fa-ellipsis-h">&nbsp;&nbsp;More</i>
+            </div>
+          </section>
+        </div>
+        <div className="main_body"></div>
+      </div>
       <div className="footer"></div>
     </div>
   );
